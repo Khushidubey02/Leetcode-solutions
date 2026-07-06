aba525aa6956417808fce4764a74b61f6ddd37fe
Khushi Dubey <khushidubey1330@gmail.com>
2026-07-06 21:37:00 +0530

    Time: 5 ms (79.94%), Space: 45.9 MB (72.19%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+
+        if(n < 0) {
+            return false;
+        }
+
+        int temp = n;
+        int rev = 0;
+
+        while(n != 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        if(temp == rev) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
